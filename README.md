83bb525d05ecc28663529773dbc2832d61504bc1
hurfy <hurfydev@gmail.com>
2024-08-25 22:45:43 +0300

    feat: new image, endpoint, rm google analytics

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-<img width="100%" src="https://hurfy.vercel.app/api?v=2"/>
+<img width="100%" src="https://hurfy.vercel.app/api/banner?v=2"/>
--- pages/api/banner/index.js
+++ pages/api/banner/index.js
@@ -1,10 +1,7 @@
 import {format} from 'date-fns'
-import image from '../../utils/image.base64'
-import ua from 'universal-analytics'
+import image from '../../../utils/image.base64'
 
 export default async (req, res) => {
-    ua('UA-76312016-3').pageview("/api").send()
-
     res.statusCode = 200
     res.setHeader('Content-Type', 'image/svg+xml')
     res.setHeader('Cache-Control', 'max-age=3600');
@@ -38,19 +35,19 @@ export default async (req, res) => {
             <!-- <rect x="16" y="185" width="380" height="23" fill="black"/> -->
             <!-- <rect x="115" y="185" width="180" height="23" fill="white"/> -->
 
-            <text class="text" x="50%" y="240" text-anchor="middle" font-size="64px" style="opacity: 0; text-shadow: 0 0 20px rgba(0, 0, 0, 0.7)" fill="#fff" font-weight="bold" id="name_d1">
-                Hi there👋
+            <text class="text" x="50%" y="250" text-anchor="middle" font-size="64px" style="opacity: 0; text-shadow: 0 0 20px rgba(0, 0, 0, 0.7)" fill="#fff" font-weight="bold" id="name_d1">
+                Hey there👋
             </text>
-            <text class="text" x="50%" y="296" text-anchor="middle" font-size="36px" style="opacity: 0" fill="#fff" id="name_d2">
-                My name is Ilya Grobovskikh!
+            <text class="text" x="50%" y="306" text-anchor="middle" font-size="36px" style="opacity: 0; text-shadow: 0 0 20px rgba(0, 0, 0, 0.7)" fill="#fff" id="name_d2">
+                My name is Ilya Grobovskikh.
             </text>
-            <text class="text" x="50%" y="340" text-anchor="middle" font-size="36px" style="opacity: 0" fill="#fff" id="name_d3">
+            <text class="text" x="50%" y="350" text-anchor="middle" font-size="36px" style="opacity: 0; text-shadow: 0 0 20px rgba(0, 0, 0, 0.7)" fill="#fff" id="name_d3">
                 I do software engineering!
             </text>
 
             <text class="text" x="30" y="42" font-size="24px" fill="#fff" style="text-shadow: 0 0 20px rgba(0, 0, 0, 0.7)">${format(new Date(), "iii, MMM. do, yyy")}</text>
 
-            <animate xlink:href="#name_d1" attributeType="CSS" attributeName="opacity" from="0" begin="3.7s" to="100" dur="0.1s" repeatCount="1" fill="freeze" />
+            <animate xlink:href="#name_d1" attributeType="CSS" attributeName="opacity" from="0" begin="3.65s" to="100" dur="0.1s" repeatCount="1" fill="freeze" />
             <animate xlink:href="#name_d2" attributeType="CSS" attributeName="opacity" from="0" begin="6s" to="1" dur="1s" repeatCount="1" fill="freeze"  />
             <animate xlink:href="#name_d3" attributeType="CSS" attributeName="opacity" from="0" begin="7s" to="1" dur="1s" repeatCount="1" fill="freeze"  />
         </svg>
